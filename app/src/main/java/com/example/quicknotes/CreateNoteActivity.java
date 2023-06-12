3342a27d5df63a02e6448608a75c3a38fb75fd97
Pranav <prparaskar211@gmail.com>
2023-06-13 00:38:39 +0530

    version 1.01: first page design only with create note design.

--- app/src/main/java/com/example/quicknotes/CreateNoteActivity.java
+++ app/src/main/java/com/example/quicknotes/CreateNoteActivity.java
@@ -3,6 +3,8 @@ package com.example.quicknotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class CreateNoteActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class CreateNoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_note);
+
+        ImageView returnbutton=findViewById(R.id.return_from_createnote);
+
+        returnbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/quicknotes/MainActivity.java
+++ app/src/main/java/com/example/quicknotes/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.quicknotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE_RETURN_BUTTON=1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ImageView addnotemainactivityimage=findViewById(R.id.addnotebutton);
+
+
+
+
+        addnotemainactivityimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               startActivityForResult(
+                       new Intent(getApplicationContext(), CreateNoteActivity.class), REQUEST_CODE_RETURN_BUTTON
+
+               );
+
+
+            }
+        });
     }
 }
