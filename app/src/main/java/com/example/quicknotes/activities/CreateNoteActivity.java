ba0354a075080fda64298d83fde7faaebaea8931
Pranav <prparaskar211@gmail.com>
2023-07-18 14:23:45 +0530

    version 1.02: first page design only with create note design. Database setup.

--- app/src/main/java/com/example/quicknotes/activities/CreateNoteActivity.java
+++ app/src/main/java/com/example/quicknotes/activities/CreateNoteActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.quicknotes.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class CreateNoteActivity extends AppCompatActivity {
+
+    private EditText inputNoteTitle, inputNoteSubtitle, inputNoteText;
+    private TextView textdatetime;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +29,10 @@ public class CreateNoteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_note);
 
         ImageView returnbutton=findViewById(R.id.return_from_createnote);
+        inputNoteText=findViewById(R.id.noteinput);
+        inputNoteTitle=findViewById(R.id.note_title);
+        inputNoteSubtitle=findViewById(R.id.createnote_subtitle);
+        textdatetime=findViewById(R.id.textdateandtime);
 
         returnbutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,5 +40,13 @@ public class CreateNoteActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+
+        textdatetime.setText(
+                new SimpleDateFormat("EEEE, dd MMMM yyyy HH:mm a", Locale.getDefault()).format(new Date())
+        );
+
+
+
     }
 }
